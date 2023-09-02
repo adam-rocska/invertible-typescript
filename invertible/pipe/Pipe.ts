3af5231655eb8f371053e6c3f68eebb516fba89d
Ádám László Rocska <adam-rocska@users.noreply.github.com>
2023-09-02 19:10:52 +0200

    I guess it's ok, time to test.

--- invertible/pipe/Pipe.ts
+++ invertible/pipe/Pipe.ts
@@ -1,5 +1,5 @@
 import {InputOf, Invertible, OutputOf, Proverse, isInvertible} from "#main";
-import {First, IsNonEmptyOf, Last, NonEmpty, NonEmptyOf, Tuple, tuple} from "#utility";
+import {First, IsNonEmptyOf, Last, NonEmpty, NonEmptyOf, Tuple} from "#utility";
 import {AreConsecutive} from "./AreConsecutive";
 import {Composition} from "./Composition";
 import {Pipeline} from "./Pipeline";
@@ -14,7 +14,7 @@ export type Pipe<ToTasks extends Proverse[]> = <
   ...tasks: AreConsecutive<[...ToTasks, ...Tasks]> extends true ? Tasks : never
 ) => Pipeline<[...ToTasks, ...Tasks]>;
 
-export const compose = <
+const compose = <
   ToTasks extends Proverse[]
 >(
   ...toTasks: ToTasks
@@ -27,7 +27,7 @@ export const compose = <
     type Input = InputOf<First<Pipeline>>;
     type Output = OutputOf<Last<Pipeline>>;
 
-    const pipeline: Pipeline = tuple(...toTasks, ...tasks);
+    const pipeline: Pipeline = Tuple(...toTasks, ...tasks);
     let composition: Composition<Pipeline>;
 
     if (pipeline.every(isInvertible) && IsNonEmptyOf(pipeline)) {
@@ -46,26 +46,3 @@ export const compose = <
   };
 
 export const pipe = compose();
-
-pipe(
-  Proverse<string, number>(async input => input.length),
-  Proverse<number, string>(async input => input.toString()),
-  Proverse<string, number>(async input => input.length),
-).pipe(
-  Proverse<number, string>(async input => input.toString()),
-  Proverse<string, number>(async input => input.length),
-  Proverse<number, string>(async input => input.toString()),
-  pipe(
-    Proverse<string, number>(async input => input.length),
-  ),
-);
-
-type debug1 = AreConsecutive<Tuple<[
-  Proverse<string, number>,
-  Proverse<number, string>,
-]>>;
-type debug2 = NonEmpty<[
-  Proverse<string, number>,
-  Proverse<number, string>,
-]>;
-type debug3 = InputOf<First<debug2>>;
--- invertible/utility/Tuple.ts
+++ invertible/utility/Tuple.ts
@@ -1,6 +1,6 @@
 import {NonEmptyOf} from "./NonEmpty";
 
 export type Tuple<Tuple extends NonEmptyOf<any>> = Tuple;
-export const tuple = <Tuple extends NonEmptyOf<any>>(...tuple: Tuple): Tuple => tuple;
+export const Tuple = <Tuple extends NonEmptyOf<any>>(...tuple: Tuple): Tuple => tuple;
 
 // TODO: if/when an isTuple will be handy, I've got a neato type-strict idea for it

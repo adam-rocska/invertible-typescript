528ed570c055c8caf6f3640ea6d6d4af4d8d1eb8
Ádám László Rocska <adam-rocska@users.noreply.github.com>
2023-09-06 14:33:44 +0200

    the interface is useless in action, it only helped to design

--- invertible/promise/InvertiblePromise.ts
+++
@@ -1,8 +0,0 @@
-import {Consecutive} from "#pipe";
-import {Inverted} from "#utility";
-
-export default interface InvertiblePromise<
-  Tasks extends Consecutive
-> {
-  invert(): InvertiblePromise<Inverted<Tasks>>
-}
--- invertible/promise/Promise.ts
+++ invertible/promise/Promise.ts
@@ -1,15 +1,14 @@
 import {OutputOf} from "#main";
 import {Consecutive} from "#pipe";
 import {Inverted, Last} from "#utility";
-import InvertiblePromise from "./InvertiblePromise";
 import NativePromise from "./NativePromise";
 
 export default
   class Promise<Tasks extends Consecutive>
   extends NativePromise<OutputOf<Last<Tasks>>>
-  implements InvertiblePromise<Tasks> {
+{
 
-  invert(): InvertiblePromise<Inverted<Tasks>> {
+  invert(): Promise<Inverted<Tasks>> {
     throw new Error("Method not implemented.");
   }
 

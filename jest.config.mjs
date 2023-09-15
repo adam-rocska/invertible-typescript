792a61b5f762fd021766815555bd7658f561a905
Ádám László Rocska <adam-rocska@users.noreply.github.com>
2023-09-15 21:58:02 +0200

    At least the tests run again

--- jest.config.mjs
+++ jest.config.mjs
@@ -7,6 +7,7 @@ if (process.env.TEST_AGAINST_ARTIFACTS) {
   execSync(`npm run clean`);
   execSync(`npm run build`);
 } else {
+  // Yes, I know there is ts-jest and its brilliant pathsToModuleNameMapper, but package.json based assures more
   moduleNameMapper = Object.fromEntries(
     Object
       .entries(packageJson.exports)
@@ -15,13 +16,21 @@ if (process.env.TEST_AGAINST_ARTIFACTS) {
         `^` + name.replace(/^./, `@21gram-consulting/invertible`) + `$`,
         entry
           .import
-          .replace(/^.\/lib\//, `<rootDir>`)
-          .replace(/\.js$/, `.ts`)
+          .replace(/^.\/lib\//, `<rootDir>/src/`)
+          .replace(/\.[mc]{0,1}js$/, `.ts`)
       ])
+      .map(([from, to]) => [
+        [from, to],
+        [
+          '^#' + from.split('/').pop().replace('$', '/(.*)$'),
+          to.replace('index.ts', '$1')
+        ]
+      ])
+      .reduce((a,b) => a.concat(b))
+      .sort()
   )
 }
 
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
   testEnvironment: `node`,
   setupFilesAfterEnv: [`<rootDir>/test/expect.ts`],
